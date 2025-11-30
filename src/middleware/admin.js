8cfda49729f1bfb2b755b30c08029d0affea4bc8
codax69 <pritbusiness03@gmail.com>
2025-11-30 12:11:07 +0530

    fix cologs

--- src/middleware/admin.js
+++ src/middleware/admin.js
@@ -6,7 +6,7 @@ const adminMiddleware = (req, res, next) => {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "").trim();
-      clg(token)
+  console.log(token)
     if (!token) {
       return res
         .status(401)
